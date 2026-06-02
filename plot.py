e2e0d3f317fc5d65e33bd2d973e6528b1d52dab2
Pierre Ballif <pierre@ballif.eu>
2026-06-02 13:50:30 +0200

    translate plot.py to JAX

--- plot.py
+++ plot.py
@@ -13,7 +13,6 @@ plt.rcParams['text.latex.preamble']=r"\usepackage{amsmath}"
 import networkx as nx
 import numpy as np
 import pickle
-import torch
 import os
 from itertools import cycle
 import argparse
@@ -42,18 +41,16 @@ with open(args.input_file, 'rb') as f:
     distance_from_optimal_tvar, edge_to_index, N_iter_per_timestep ] = pickle.load(f)
 
 
-N_tests = distance_from_optimal_tvar.size(0)
-T = distance_from_optimal_tvar.size(2)
+N_tests = distance_from_optimal_tvar.shape[0]
+T = distance_from_optimal_tvar.shape[2]
 Steps_between_iterations = 10
-N_iterations = residual_store.size(1)
+N_iterations = residual_store.shape[1]
 
-
-torch.Tensor.ndim = property(lambda self: len(self.shape))  # Necessary to use matplotlib with tensors
 
 #### Plot 1: residual static case
 fig, ax = plt.subplots(1, 1, figsize=(4, 2.1), sharex='col')
 x =  range(1, Steps_between_iterations * N_iterations, Steps_between_iterations)
-ax.plot(x, torch.mean(residual_store, dim=0))
+ax.plot(x, np.mean(residual_store, axis=0))
 ax.grid(True)
 ax.set_yscale('log')
 # ax.set_xscale('log')
@@ -76,7 +73,7 @@ x =  range(T)
 lines = ["-", "--", "-.", ":"]
 linecycler = cycle(lines)
 for index_K in range(len(N_iter_per_timestep)):
-    ax.plot(x, torch.mean(distance_from_optimal_tvar[:,index_K,:], dim=0), label=str(N_iter_per_timestep[index_K]),linestyle=next(linecycler))
+    ax.plot(x, np.mean(distance_from_optimal_tvar[:,index_K,:], axis=0), label=str(N_iter_per_timestep[index_K]),linestyle=next(linecycler))
 ax.grid(True)
 ax.set_ylabel(r'$\|x_t - x_t^*\|$', fontsize=9)
 ax.set_xlabel("Time-step", fontsize=9)
@@ -99,7 +96,7 @@ x =  range(T)
 lines = ["-", "--", "-.", ":"]
 linecycler = cycle(lines)
 for index_K in range(len(N_iter_per_timestep)):
-    ax.plot(x, torch.mean(shared_const_viol_tvar[:,index_K,:]+loc_const_viol_tvar[:,index_K,:], dim=0),\
+    ax.plot(x, np.mean(shared_const_viol_tvar[:,index_K,:]+loc_const_viol_tvar[:,index_K,:], axis=0),\
         label=str(N_iter_per_timestep[index_K]),linestyle=next(linecycler))
 ax.grid(True)
 ax.set_ylabel(r'$\|Ax_t - b_t\|$', fontsize=9)
